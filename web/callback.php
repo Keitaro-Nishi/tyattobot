02026b561559265b1b878282a00c3744cb9c2a56
Keitaro_Nishizawa <Keitaro_Nishizawa@DESKTOP-TU4HREO>
2017-07-12 16:20:07 +0900

    x

--- web/callback.php
+++ web/callback.php
@@ -166,61 +166,65 @@ if ($type != "text") {
 	error_log ( $messageId );
 	// ↓コメ
 
-	// 画像ファイルのバイナリ取得
-	$ch = curl_init ( "https://api.line.me/v2/bot/message/reply" . $messageId . "/content" );
-	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-	curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
-			'Content-Type: application/json; charser=UTF-8',
-			'Authorization: Bearer ' . $accessToken
-	) );
-	$result = curl_exec ( $ch );
-
-	error_log ( $result );
-
-	curl_close ( $ch );
-
-	$fp = fopen ( "'https://" . $_SERVER ['SERVER_NAME'] . "/test.jpg'", 'wb' );
-
-	if ($fp) {
-		if (flock ( $fp, LOCK_EX )) {
-			if (fwrite ( $fp, $result ) === FALSE) {
-				error_log ( 'ファイル書き込みに失敗しました' );
-			} else {
-				error_log ( 'ファイルに書き込みました' );
-			}
-			flock ( $fp, LOCK_UN );
-		} else {
-			error_log ( 'ファイルロックに失敗しました' );
-		}
-	}
-
-	fclose ( $fp );
-
-	// ↑コメ
 
 
-	 // そのまま画像をオウム返しで送信
-	 $response_format_text = [
-	 "type" => "image",
-	 "originalContentUrl" => "https://" . $_SERVER ['SERVER_NAME'] . "/rabbit.jpg",
-	 "previewImageUrl" => "https://" . $_SERVER ['SERVER_NAME'] . "/rabbit.jpg"
-	 ];
+	 // 画像ファイルのバイナリ取得
+	 $ch = curl_init ( "https://api.line.me/v2/bot/message/reply" . $messageId . "/content" );
+	 curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+	 curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+	 'Content-Type: application/json; charser=UTF-8',
+	 'Authorization: Bearer ' . $accessToken
+	 ) );
+	 $result = curl_exec ( $ch );
+
+
+	 error_log ( $result );
+
+	 curl_close ( $ch );
+
+	 $fp = fopen ( "'https://" . $_SERVER ['SERVER_NAME'] . "/test.jpg'", 'wb' );
+
+	 if ($fp) {
+	 if (flock ( $fp, LOCK_EX )) {
+	 if (fwrite ( $fp, $result ) === FALSE) {
+	 error_log ( 'ファイル書き込みに失敗しました' );
+	 } else {
+	 error_log ( 'ファイルに書き込みました' );
+	 }
+	 flock ( $fp, LOCK_UN );
+	 } else {
+	 error_log ( 'ファイルロックに失敗しました' );
+	 }
+	 }
+
+	 fclose ( $fp );
+
+	 //↑コメ
+
+
+	// そのまま画像をオウム返しで送信
+	$response_format_text = [
+			"type" => "image",
+			"originalContentUrl" => "https://" . $_SERVER ['SERVER_NAME'] . "/rabbit.jpg",
+			"previewImageUrl" => "https://" . $_SERVER ['SERVER_NAME'] . "/rabbit.jpg"
+	];
+
 /*
-	 //エラーログの代わり
-	 $response_format_text = [
-	 "type" => "text",
-	 "text" => $jsonObj
-	 //"text" => $result
-	 ];
-
-	$post_data = [
+//エラーログの代わり
+	 	 $response_format_text = [
+			"type" => "text",
+			"text" => $jsonObj
+			//"text" => $result
+	];
+*/
+	 	 $post_data = [
 			"replyToken" => $replyToken,
 			"messages" => [
 					$response_format_text
 			]
 
 	];
-*/
+
 	$ch = curl_init ( "https://api.line.me/v2/bot/message/reply" );
 	curl_setopt ( $ch, CURLOPT_POST, true );
 	curl_setopt ( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
@@ -235,28 +239,36 @@ if ($type != "text") {
 	curl_close ( $ch );
 	error_log ( 画像送信 );
 
-	// Visual Recognition
-	/*
-	 * $classfier = "12d0fcx34-nlc-410";
-	 * $workspace_id ="";
-	 *
-	 * $url = "https://gateway-a.watsonplatform.net/visual-recognition/api/workspaces/". $workspace_id . "";
-	 *
-	 *
-	 * $data = array (
-	 * 'input' => array (
-	 * "image" => $image
-	 * )
-	 * );
-	 *
-	 *
-	 *
-	 *
-	 */
+	//Visual Recognition
+/*
+	$classfier = "12d0fcx34-nlc-410";
+	$workspace_id ="";
+
+	$url = "https://gateway-a.watsonplatform.net/visual-recognition/api/workspaces/". $workspace_id . "";
+
+
+	$data = array (
+			'input' => array (
+					"image" => $image
+			)
+	);
+
+
+
+
+*/
 	exit ();
 }
 
-// Conversation用
+
+
+
+
+
+
+
+
+//Conversation用
 
 $classfier = "12d0fcx34-nlc-410";
 $workspace_id = "4c2bcc67-db84-438e-b20d-c1d76e143a68";
