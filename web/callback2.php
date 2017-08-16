b4452f66813806445cc09c6ee29f555a92659f27
Keitaro_Nishizawa <Keitaro_Nishizawa@DESKTOP-TU4HREO>
2017-08-16 10:41:03 +0900

    g

--- web/callback2.php
+++ web/callback2.php
@@ -7,28 +7,28 @@
 $conn = "host=ec2-54-83-26-65.compute-1.amazonaws.com dbname=ec2-54-83-26-65.compute-1.amazonaws.com user=gopasxxhdasfak password=ab14f9f8cbd407f8e7c7c99d3d03ac82f3c35b9d7a141615a563adeb2dd964f4";
 $link = pg_connect($conn);
 if (!$link) {
-    die('接続失敗です。'.pg_last_error());
+	error_log(接続失敗です。);
 }
 
-print('接続に成功しました。<br>');
+error_log(接続に成功しました。);
 
 pg_set_client_encoding("git");
 
 $result = pg_query('SELECT contents FROM botlog');
 if (!$result) {
-    die('クエリーが失敗しました。'.pg_last_error());
+	error_log(クエリーが失敗しました。);
 }
 
 for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
     $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-    print('id='.$rows['id']);
-    print(',name='.$rows['name'].'<br>');
+    error_log($rows[USERID]);
+    error_log($rows[CONTENTS]);
 }
 
 $close_flag = pg_close($link);
 
 if ($close_flag){
-    print('切断に成功しました。<br>');
+	error_log(切断に成功しました。);
 }
 
 ?>
@@ -41,8 +41,8 @@ if ($close_flag){
  while($table = mysql_fetch_assoc($recordset)) {
  ?>
  <tr>
-  <td><?php print(htmlspecialchars($table['id'])); ?> </td>
-  <td><?php print(htmlspecialchars($table['maker'])); ?> </td>
+  <td><?php print(htmlspecialchars($table['USERID'])); ?> </td>
+  <td><?php print(htmlspecialchars($table['CONTENTS'])); ?> </td>
  </tr>
  <?php
  }
