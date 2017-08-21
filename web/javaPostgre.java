5782ff0b32e2a23f5af6671a647f4990fbe12b38
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-08-21 15:33:38 +0900

    u

---
+++ web/javaPostgre.java
@@ -0,0 +1,20 @@
+package log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class javaPostgre {
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection("jdbc:postgresql://ec2-54-83-26-65.compute-1.amazonaws.com"
+					+ ":5432/d9pf8qthde7brb", "gopasxxhdasfak", "ab14f9f8cbd407f8e7c7c99d3d03ac82f3c35b9d7a141615a563adeb2dd964f4");
+			conn.close();
+			System.out.println("OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("NG");
+		}
+	}
+}
