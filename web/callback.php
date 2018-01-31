4c34558efdfeb5733456be5785a3319407932955
Keitaro <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-31 14:52:31 +0900

    ｓ

--- web/callback.php
+++ web/callback.php
@@ -27,7 +27,7 @@ $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッ�
 if($message_type != "text") exit;
  
 //返信メッセージ
-$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+$return_message_text = $message_text . "じゃねーよｗｗｗ";
  
 //返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
